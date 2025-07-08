d14730fa072d3cb303a9ae71aa3938e13fd70403
enrate <80484441+enrate@users.noreply.github.com>
2025-07-09 02:11:48 +0300

    Update 20250706215954-add-invite-events-to-squad-history.js

--- server/migrations/20250706215954-add-invite-events-to-squad-history.js
+++ server/migrations/20250706215954-add-invite-events-to-squad-history.js
@@ -1,19 +1,19 @@
-'use strict';
+// 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    // Добавляем новые значения в ENUM
-    await queryInterface.sequelize.query(`
-      ALTER TYPE "enum_squad_history_eventType" ADD VALUE 'invite';
-    `);
-    await queryInterface.sequelize.query(`
-      ALTER TYPE "enum_squad_history_eventType" ADD VALUE 'invite_accept';
-    `);
-  },
+// /** @type {import('sequelize-cli').Migration} */
+// module.exports = {
+//   up: async (queryInterface, Sequelize) => {
+//     // Добавляем новые значения в ENUM
+//     await queryInterface.sequelize.query(`
+//       ALTER TYPE "enum_squad_history_eventType" ADD VALUE 'invite';
+//     `);
+//     await queryInterface.sequelize.query(`
+//       ALTER TYPE "enum_squad_history_eventType" ADD VALUE 'invite_accept';
+//     `);
+//   },
 
-  down: async (queryInterface, Sequelize) => {
-    // В PostgreSQL нельзя удалить значения из ENUM, поэтому просто логируем
-    console.log('Cannot remove ENUM values in PostgreSQL. Manual cleanup may be required.');
-  }
-};
+//   down: async (queryInterface, Sequelize) => {
+//     // В PostgreSQL нельзя удалить значения из ENUM, поэтому просто логируем
+//     console.log('Cannot remove ENUM values in PostgreSQL. Manual cleanup may be required.');
+//   }
+// };
