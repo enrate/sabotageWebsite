99fbdefc8358add01ebf655d14470873bd793e38
enrate <80484441+enrate@users.noreply.github.com>
2025-07-12 00:25:57 +0300

    Update 20250115000000-add-email-verification-to-users.js

--- server/migrations/20250115000000-add-email-verification-to-users.js
+++ server/migrations/20250115000000-add-email-verification-to-users.js
@@ -2,26 +2,30 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('users', 'emailVerified', {
-      type: Sequelize.BOOLEAN,
-      allowNull: false,
-      defaultValue: false,
-      comment: 'Флаг подтверждения email'
-    });
-
-    await queryInterface.addColumn('users', 'emailVerificationToken', {
-      type: Sequelize.STRING,
-      allowNull: true,
-      comment: 'Токен для подтверждения email'
-    });
-
-    await queryInterface.addColumn('users', 'emailVerificationExpires', {
-      type: Sequelize.DATE,
-      allowNull: true,
-      comment: 'Срок действия токена подтверждения email'
-    });
+    const table = await queryInterface.describeTable('users');
+    if (!table.emailVerified) {
+      await queryInterface.addColumn('users', 'emailVerified', {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+        comment: 'Флаг подтверждения email'
+      });
+    }
+    if (!table.emailVerificationToken) {
+      await queryInterface.addColumn('users', 'emailVerificationToken', {
+        type: Sequelize.STRING,
+        allowNull: true,
+        comment: 'Токен для подтверждения email'
+      });
+    }
+    if (!table.emailVerificationExpires) {
+      await queryInterface.addColumn('users', 'emailVerificationExpires', {
+        type: Sequelize.DATE,
+        allowNull: true,
+        comment: 'Срок действия токена подтверждения email'
+      });
+    }
   },
-
   down: async (queryInterface, Sequelize) => {
     await queryInterface.removeColumn('users', 'emailVerified');
     await queryInterface.removeColumn('users', 'emailVerificationToken');
