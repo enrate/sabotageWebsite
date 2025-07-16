33fdf7580d47b31e20e967fafd1c0bb08ac7e987
enrate <80484441+enrate@users.noreply.github.com>
2025-07-16 21:09:34 +0300

    pd

--- client/src/admin/src/index.js
+++ client/src/admin/src/index.js
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AdminAuthProvider } from "./context/AdminAuthContext";
+import AdminLoginPage from "./pages/AdminLoginPage";
 import AdminDashboard from "./AdminDashboard";
+import AdminPrivateRoute from "./components/AdminPrivateRoute";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AdminDashboard />
+    <AdminAuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<AdminLoginPage />} />
+          <Route path="/*" element={
+            <AdminPrivateRoute>
+              <AdminDashboard />
+            </AdminPrivateRoute>
+          } />
+        </Routes>
+      </BrowserRouter>
+    </AdminAuthProvider>
   </React.StrictMode>
 ); 
