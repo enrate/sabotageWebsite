d8e916a2429b96b8edc880310028515214ee7fa7
enrate <80484441+enrate@users.noreply.github.com>
2025-07-17 15:27:17 +0300

    Update DashboardLayout.js

--- admin/src/components/DashboardLayout.js
+++ admin/src/components/DashboardLayout.js
@@ -4,12 +4,12 @@ import CssBaseline from '@mui/material/CssBaseline';
 import AppNavbar from './AppNavbar';
 import SideMenu from './SideMenu';
 
-export default function DashboardLayout({ section, setSection, children }) {
+export default function DashboardLayout({ section, navigate, children }) {
   return (
     <Box sx={{ display: 'flex', minHeight: '100vh', bgcolor: 'background.default' }}>
       <CssBaseline />
       <AppNavbar />
-      <SideMenu section={section} setSection={setSection} />
+      <SideMenu section={section} navigate={navigate} />
       <Box component="main" sx={{ flexGrow: 1, p: { xs: 1, sm: 3 }, width: { sm: `calc(100% - 240px)` }, mt: 8, minHeight: '100vh', bgcolor: 'background.default' }}>
         {children}
       </Box>
